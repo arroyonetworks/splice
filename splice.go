77409c695adab9ff3be6302cfe08d078b56daa7d
Matthew Ellison <matt@arroyonetworks.com>
2017-05-08 12:41:41 -0400

    Adds Package Descriptions

---
+++ splice.go
@@ -0,0 +1,8 @@
+// Copyright 2017 Arroyo Networks, LLC. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Provides a high-level library for manipulating network interfaces, links,
+// and routes. Splice provides a unified interface for multiple operating
+// systems.
+package splice
--- splice_test.go
+++ splice_test.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Provides unit tests for the Splice library.
 package splice_test
 
 import (
